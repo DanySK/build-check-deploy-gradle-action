c117d0d9a8b402d6a5fdb0281463b1b7ef793fba
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-07-10 11:58:55 +0200

    fix: move to mjs modules for semantic release config file (#644)

---
+++ release.config.mjs
@@ -0,0 +1,9 @@
+import config from 'semantic-release-preconfigured-conventional-commits' assert { type: "json" }
+
+config.plugins.push(
+    "@semantic-release/github",
+    "@semantic-release/git",
+)
+
+
+export default config;
